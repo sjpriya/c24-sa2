d8eb65f2c5d8c1a2038a40d61662713a554db6c0
sjpriya <72385035+sjpriya@users.noreply.github.com>
2021-01-12 16:35:24 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,20 +15,7 @@ function setup(){
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
- //   pig1 = new Pig(810, 350);
- //   log1 = new Log(810,260,300, PI);
 
- //   box3 = new Box(700,240,70,70);
- //   box4 = new Box(920,240,70,70);
- //   pig3 = new Pig(810, 220);
-
- //   log3 =  new Log(810,180,300, PI);
-
- //   box5 = new Box(810,160,70,70);
-  //  log4 = new Log(760,120,150, PI/7);
-  //  log5 = new Log(870,120,150, -PI/7);
-
- //   bird = new Bird(100,100);
 
 }
 
@@ -40,17 +27,5 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
- //   pig1.display();
- //   log1.display();
 
- //   box3.display();
- //   box4.display();
- //   pig3.display();
- //   log3.display();
-
- //   box5.display();
-  //  log4.display();
-  //  log5.display();
-
-  //  bird.display();
 }
